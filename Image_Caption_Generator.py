b3c9e736d1f0864733139e515230b38dbe0de269
Lewis <70917466+LewisJ03@users.noreply.github.com>
2025-05-05 18:33:08 +0100

    fixed inceptionV3

--- Image_Caption_Generator.py
+++ Image_Caption_Generator.py
@@ -22,28 +22,28 @@ def randImage():
     imageDir=os.path.join(fileDir,randFile)
 
     os.startfile(imageDir)#opens file of random image
-    captionCheck(randFile)
-    modelSwitch(imageDir)
+    realCaption=captionCheck(randFile)
+    modelSwitch(imageDir,realCaption)
     menu()
 
 def upload():
     try:
         imageDir=input("\nCopy image directory here\n")
-        caption=input("\nplease enter a descriptive sentence about the image\n")
-        print(caption)#takes image directory and makes the user input a caption about the image
+        realCaption=input("\nplease enter a descriptive sentence about the image\n")
+        print(realCaption)#takes image directory and makes the user input a caption about the image
 
         os.startfile(imageDir)
-        modelSwitch(imageDir)
+        modelSwitch(imageDir,realCaption)
         menu()
     except:
         print("\nFile cannot be found")
         menu()
 
-def modelSwitch(imageDir):
+def modelSwitch(imageDir,realCaption):
     modelMenu=int(input("\ntype 1 for or type 2 for Blip\n"))#user can choose which model to use
     match modelMenu:
         case 1:
-            inception(imageDir)#inception and blip for encode-decode(actual module path)
+            inceptionv3(imageDir,realCaption)#inception and blip for encode-decode(actual module path)
         case 2:
             blip(imageDir)#accurate for testing/ all in one model
         case _:
@@ -65,45 +65,43 @@ def captionCheck(randFile):
         for lineNum, line in enumerate(file, start=2):#first line is name so skip
             if randFile in line.strip():#checks for file name in caption file
                 caption=(line.strip()).replace((randFile+","),"")
-                caption=caption.replace(" .","")#removes all but caption from descriptive string
-    print("\n",caption,"\n")
+                realCaption=caption.replace(" .","")#removes all but caption from descriptive string
+    print("\n",realCaption,"\n")
+    return realCaption
 
-
-def inception(imageDir):
+def inceptionv3(imageDir,realCaption):
     model = models.inception_v3(pretrained=True)#setting up model
     model.eval()
-
     image = Image.open(imageDir)
+
     reSize = transforms.Compose([
-    transforms.Resize(299), 
-    transforms.CenterCrop(299),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # resizes to fit inceptionv3 requirements
-    ])
-    
+    transforms.Resize(299),transforms.CenterCrop(299),transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]) # resizes to fit inceptionv3 requirements
+
     tensor = reSize(image)
     unSqueezed = tensor.unsqueeze(0)#correcting format
 
     with torch.no_grad():
         outputs = model(unSqueezed)
 
-    probability = torch.nn.functional.softmax(outputs[0], dim=0) 
-    predictions = 3  #top 3 predictions
-    indices = torch.topk(probability, predictions)
+    probability = torch.nn.functional.softmax(outputs[0], dim=0)
+    predictions = 3#top 3 predictions
+    values, indices = torch.topk(probability, predictions)
 
     _, predicted_class = torch.max(outputs, 1)
+
     LABELS_URL = "https://storage.googleapis.com/download.tensorflow.org/data/imagenet_class_index.json"
-    labelJson = requests.get(LABELS_URL).json()#returns a json string with probabilties of predictions
+    labels = requests.get(LABELS_URL).json()#returns a json string with probabilties of predictions
 
     captionWords=[]
     for i in range(predictions):
-        index = indices[i].item()
-        label = labelJson[str(index)][1]
+        idx = indices[i].item()
+        label = labels[str(idx)][1]
         captionWords.append(label)#saves top predictions to list
         
-    decoderBlip(captionWords)
+    decoderBlip(captionWords,realCaption)
 
-def decoderBlip(captionWords): 
+def decoderBlip(captionWords,realCaption): 
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")#setting up links for model
 
@@ -117,8 +115,21 @@ def decoderBlip(captionWords):
 
         caption=processor.decode(output[0],skip_special_tokens=True)
         descriptors=caption.split(':')
-        if descriptors[1]=="":#loops until returns no empty
-            decoderBlip(captionWords)
+        descriptVerify=descriptors[1]
+        if descriptVerify=="":#loops until returns no empty
+            decoderBlip(captionWords,realCaption)
         print("\n",caption,"\n")
+    captionVerify(descriptVerify,realCaption)
+
+
+def captionVerify(descriptVerify,realCaption):
+    descriptArr=descriptVerify.split()
+
+    count=0
+    for x in range(len(descriptArr)):
+        if descriptArr[x] in realCaption:
+            count+=1
+    
+    print("prompt accuracy:",count)
 
 menu()
